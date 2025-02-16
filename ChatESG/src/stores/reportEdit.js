ee2de638e766867dbb2c14cfbd3078f1e55726d4
Lian <laix678@gmail.com>
2025-02-16 23:27:34 +0800

    Update reportEdit.js

--- ChatESG/src/stores/reportEdit.js
+++ ChatESG/src/stores/reportEdit.js
@@ -733,7 +733,7 @@ export const useReportEditStore = defineStore('reportEdit', {
         toast.success('審核送出成功')
       } catch (error) {
         console.error('審核流程時發生錯誤:', error);
-        toast.error('審核流程時發生錯誤:' + error)
+        toast.error('審核流程時發生錯誤:' + error.message.replace('建立審核流程實例失敗: 400: 該章節 ', ''))
         throw error;
       }
     },
